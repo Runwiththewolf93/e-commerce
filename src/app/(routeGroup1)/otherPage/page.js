78a8a2639e9946fffcd7e8306d23d068bbbf9c5b
Runwiththewolf93 <stevan38a@gmail.com>
2023-09-06 18:59:58 +0200

    basic setup

--- src/app/(routeGroup1)/otherPage/page.js
+++ src/app/(routeGroup1)/otherPage/page.js
@@ -1,4 +1,3 @@
 export default function Page() {
-    return <h1>This is a page in routeGroup1</h1>;
-  }
-  
+  return <h1>This is a page in routeGroup1</h1>;
+}
---
+++ src/app/(routeGroup2)/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      <h1>Layout for routeGroup2</h1>
+      {children}
+    </div>
+  );
+}
