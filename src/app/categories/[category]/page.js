953789debc07d5434038d23c5376f15c3d2376cb
Runwiththewolf93 <stevan38a@gmail.com>
2023-10-25 17:07:04 +0200

    implemented price range

--- src/app/categories/[category]/page.js
+++ src/app/categories/[category]/page.js
@@ -2,34 +2,32 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategory } from "../../../redux/slices/productSlice";
 import Link from "next/link";
 import { BreadCrumb, DropDown } from "../components/CategoryComponents";
+import RangeSliderElement from "../components/CategoryComponents";
 
 export default function Category() {
   const dispatch = useDispatch();
   const { isLoadingCategory, productsCategory, errorCategory } = useSelector(
     state => state.products
   );
-
-  // For testing purposes only
-  const generateObjectID = () => {
-    return Math.random().toString(16).substr(2, 24);
-  };
-
-  // Duplicate the products 15 times
-  const duplicatedProducts = Array(15)
-    .fill(productsCategory)
-    .flat()
-    .map((product, index) => {
-      return { ...product, _id: generateObjectID() };
-    });
-
   const pathname = usePathname();
   const link = pathname.split("/").pop();
   const category = productsCategory.map(p => p.category)[0];
+  const minPrice = Math.min(...productsCategory.map(p => p.price));
+  const maxPrice = Math.max(...productsCategory.map(p => p.price));
+  const uniquePrices = [...new Set(productsCategory.map(p => p.price))];
+  // console.log(
+  //   "🚀 ~ file: page.js:18 ~ Category ~ productsCategory:",
+  //   productsCategory
+  // );
+
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [triggerValue, setTriggerValue] = useState(null);
+  const [lastCloseEnough, setLastCloseEnough] = useState(null);
 
   useEffect(() => {
     if (!productsCategory.length) {
@@ -37,23 +35,58 @@ export default function Category() {
     }
   }, [link, dispatch, productsCategory.length]);
 
+  // For testing purposes only
+  const generateObjectID = () => {
+    return Math.random().toString(16).substr(2, 24);
+  };
+  const duplicatedProducts = Array(15)
+    .fill(productsCategory)
+    .flat()
+    .map(product => {
+      return { ...product, _id: generateObjectID() };
+    });
+  // For testing purposes only
+
+  useEffect(() => {
+    if (triggerValue !== null) {
+      console.log("when does this trigger? useEffect");
+      const newFilteredProducts = duplicatedProducts.filter(product => {
+        return product.price >= minPrice && product.price <= triggerValue;
+      });
+      setFilteredProducts(newFilteredProducts);
+    } else {
+      setFilteredProducts(duplicatedProducts);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [triggerValue]);
+
   return (
     <div className="bg-white pt-5">
       <BreadCrumb category={category} />
-      <div className="grid grid-cols-2 ml-3 pt-5 max-w-max">
+      <div className="grid grid-cols-3 pt-5 max-w-max mx-auto">
         <div>
+          <h1 className="font-bold text-2xl">{category}</h1>
+          <p className="italic pt-5">
+            Browse our fine selection of {category} at your leisure:
+          </p>
+        </div>
+        <div className="justify-self-center">
           <DropDown />
         </div>
         <div>
-          <h1 className="font-bold text-2xl">{category}</h1>
-          <p className="italic pt-5 ml-3">
-            Browse our fine selection of {category} at your leisure:
-          </p>
+          <RangeSliderElement
+            setTriggerValue={setTriggerValue}
+            minPrice={minPrice}
+            maxPrice={maxPrice}
+            uniquePrices={uniquePrices}
+            lastCloseEnough={lastCloseEnough}
+            setLastCloseEnough={setLastCloseEnough}
+          />
         </div>
       </div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {duplicatedProducts.map(product => (
+          {filteredProducts.map(product => (
             <Link
               key={product._id}
               href={`/categories/${link}/${product._id}`}
--- src/app/categories/components/CategoryComponents.js
+++ src/app/categories/components/CategoryComponents.js
@@ -1,13 +1,8 @@
 "use client";
 
-import { Breadcrumb, Dropdown } from "flowbite-react";
-import {
-  HiHome,
-  HiCog,
-  HiCurrencyDollar,
-  HiLogout,
-  HiViewGrid,
-} from "react-icons/hi";
+import { Breadcrumb, Dropdown, Label, RangeSlider } from "flowbite-react";
+import { HiHome } from "react-icons/hi";
+import { useState, memo } from "react";
 
 export function BreadCrumb({ category }) {
   return (
@@ -31,3 +26,73 @@ export function DropDown() {
     </Dropdown>
   );
 }
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.lastCloseEnough === nextProps.lastCloseEnough;
+};
+
+const RangeSliderElement = ({
+  setTriggerValue,
+  minPrice,
+  maxPrice,
+  uniquePrices,
+  lastCloseEnough,
+  setLastCloseEnough,
+}) => {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  console.log(
+    "🚀 ~ file: CategoryComponents.js:37 ~ uniquePrices:",
+    uniquePrices
+  );
+
+  const handleMouseDown = () => {
+    setIsMouseDown(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
+  const tolerance = 5;
+
+  const handleSliderChange = e => {
+    if (!isMouseDown) return;
+
+    const value = parseInt(e.target.value, 10);
+
+    const closeEnough = uniquePrices.find(
+      price => Math.abs(price - value) <= tolerance
+    );
+
+    if (closeEnough !== undefined && closeEnough !== lastCloseEnough) {
+      setLastCloseEnough(closeEnough);
+      setTriggerValue(closeEnough);
+    } else if (value === maxPrice) {
+      setTriggerValue(null);
+    }
+  };
+
+  return (
+    <div>
+      <div className="mb-1 block">
+        <Label htmlFor="range" value="Price" className="text-md" />
+      </div>
+      <div className="flex items-center">
+        <span className="mr-1">€ {minPrice}</span>
+        <RangeSlider
+          id="range"
+          min={minPrice}
+          max={maxPrice}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleSliderChange}
+          onMouseUp={handleMouseUp}
+          className="flex-1"
+        />
+        <span className="ml-1">€ {maxPrice}</span>
+      </div>
+    </div>
+  );
+};
+
+export default memo(RangeSliderElement, areEqual);
